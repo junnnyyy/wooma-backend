3dffe9cff83b97c56a19d96955726c83c7751f29
Chloe Lee <junnnyyy@kakao.com>
2024-03-07 00:32:09 +0900

    initial commit

---
+++ src/main/kotlin/dotolhee/daramhee/woomabackend/WoomaBackendApplication.kt
@@ -0,0 +1,11 @@
+package dotolhee.daramhee.woomabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WoomaBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<WoomaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/dotolhee/daramhee/woomabackend/WoomaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dotolhee.daramhee.woomabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WoomaBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
